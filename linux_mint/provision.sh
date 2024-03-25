2abdf63555114ed7a9a1cd951348b24dd4d47850
Mike Di Nicola <mike@mdinicola.com>
2024-03-24 20:42:34 -0400

    update ansible_galaxy to match convention

--- linux_mint/provision.sh
+++ linux_mint/provision.sh
@@ -1,3 +1,3 @@
 sudo ls > /dev/null
-ansible-playbook -i inventory.yml playbooks/ansible-galaxy.yml
+ansible-playbook -i inventory.yml playbooks/ansible_galaxy.yml
 ansible-playbook -i inventory.yml playbooks/provision.yml
