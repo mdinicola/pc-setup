181f808d6e332e243a2ed975ced4dc642278cfd0
Mike <mike@mdinicola.com>
2026-07-24 14:12:34 -0400

    bootstrap fedora kde plasma

---
+++ linux/fedora_kde_plasma/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo dnf install -y ansible-core
+
+ansible-playbook -i inventory.yml playbooks/ansible_galaxy.yml
+ansible-playbook --inventory inventory.yml playbooks/bootstrap.yml --ask-become-pass
